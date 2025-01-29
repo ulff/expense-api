3815aa2134bc1497dac7e6965818d813392eeb4a
Olaf Galazka <olaf.galazka@schibsted.com>
2025-01-29 19:21:31 +0100

    added endpoint for getting summary for any period

--- src/routes/summary.ts
+++ src/routes/summary.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getCurrentPeriod } from "../domain/model/period";
+import { getCurrentPeriod, getPeriod } from "../domain/model/period";
 import { listExpensesForPeriod } from "../domain/model/expense";
 import { prepareSummary } from "../domain/model/summary";
 import MissingPeriodForDateError from "../domain/error/MissingPeriodForDateError";
@@ -10,6 +10,25 @@ const router = express.Router();
 router.get("/current", async (request, response) => {
   try {
     const period = await getCurrentPeriod();
+    const expenses = await listExpensesForPeriod(period);
+
+    const summary = await prepareSummary({ period, expenses, categories });
+
+    response.status(200).json(summary);
+  } catch (e) {
+    if (e instanceof MissingPeriodForDateError) {
+      response.status(404).json({ errors: e.message });
+      return;
+    }
+    response.status(500).end();
+  }
+});
+
+router.get("/:periodId", async (request, response) => {
+  const periodId = request.params.periodId;
+
+  try {
+    const period = await getPeriod(periodId);
     const expenses = await listExpensesForPeriod(period);
 
     const summary = await prepareSummary({ period, expenses, categories });
