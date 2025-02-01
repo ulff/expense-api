f045711c1b28fb1bfaf3d79376aea85cbf3a0547
Olaf Galazka <olaf.galazka@schibsted.com>
2025-02-01 13:28:48 +0100

    tested sumPLN method

--- src/domain/types/PLN.test.ts
+++ src/domain/types/PLN.test.ts
@@ -1,7 +1,7 @@
 import { sumPLN, PLN } from "./PLN";
 
-describe("sumPLN tests", () => {
-  test("adds 1.00 PLN and 2.00 PLN properly", () => {
+describe("sumPLN method tests", () => {
+  test("verifies that 1.00 PLN + 2.00 PLN is 3.00 PLN", () => {
     const a: PLN = {
       zloty: 1,
       groszy: 0,
@@ -15,6 +15,44 @@ describe("sumPLN tests", () => {
     const expectedSum: PLN = {
       zloty: 3,
       groszy: 0,
+    };
+
+    expect(sumPLN(a, b)).toStrictEqual(expectedSum);
+  });
+
+  test("verifies that 1.99 PLN + 2.99 PLN is 4.98 PLN", () => {
+    const a: PLN = {
+      zloty: 1,
+      groszy: 99,
+    };
+
+    const b: PLN = {
+      zloty: 2,
+      groszy: 99,
+    };
+
+    const expectedSum: PLN = {
+      zloty: 4,
+      groszy: 98,
+    };
+
+    expect(sumPLN(a, b)).toStrictEqual(expectedSum);
+  });
+
+  test("verifies that 3.75 PLN + 0.00 PLN is 3.75 PLN", () => {
+    const a: PLN = {
+      zloty: 3,
+      groszy: 75,
+    };
+
+    const b: PLN = {
+      zloty: 0,
+      groszy: 0,
+    };
+
+    const expectedSum: PLN = {
+      zloty: 3,
+      groszy: 75,
     };
 
     expect(sumPLN(a, b)).toStrictEqual(expectedSum);
